015b663dea6a982fb8adc4196684f62119217eff
Lindomar <lindomarbatistao@gmail.com>
2024-06-21 13:27:12 -0300

    21-06-24 v2

--- api/views.py
+++ api/views.py
@@ -22,7 +22,7 @@ def listar_clientes(request):
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         
 @api_view(['POST'])
-@permission_classes([AllowAny])
+@permission_classes([AllowAny]) #Permissão total
 def create_city(request):
     if request.method == 'POST':
         serializer = CidadeSerializer(data=request.data)
